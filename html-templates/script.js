f7710e8d84689d693d321bd8716f756f9fb9cbf6
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2017-03-12 13:29:02 -0300

    Created the first version of the tarks JS library (I'm wondering to change it's name soon) and added the first prototype of the message box function. ASAP I'm gonna refactor it and add new functions.

--- html-templates/script.js
+++ html-templates/script.js
@@ -1,13 +1,17 @@
-﻿
+﻿/// <reference path="E:\Projects\html-templates\html-templates\tarks.js" />
 
 function toggleNav() {
     if (document.getElementById("mySidenav").style.width == "250px") {
         document.getElementById("mySidenav").style.width = "0";
         document.getElementsByTagName("main")[0].style.marginLeft = "0";
+        document.getElementsByTagName("footer")[0].style.marginLeft = "0";
+        document.getElementsByTagName("nav")[0].style.marginLeft = "0px";
     }
     else {
         document.getElementById("mySidenav").style.width = "250px";
         document.getElementsByTagName("main")[0].style.marginLeft = "250px";
+        document.getElementsByTagName("footer")[0].style.marginLeft = "250px";
+        document.getElementsByTagName("nav")[0].style.marginLeft = "250px";
     }
 
 }
@@ -20,28 +24,13 @@ function myMap() {
     var map = new google.maps.Map(mapCanvas, mapOptions);
 }
 
-// Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+btn.onclick = function(event) {
+    
+    Tarks.MessageBox.Show(
+        "You have successfully signed in",
+        "Sign In",
+        Tarks.MessageBox.Icons.Success
+    );
 }
---
+++ html-templates/tarks.js
@@ -0,0 +1,101 @@
+﻿Tarks = function () {
+    
+    this.MessageBox = function () {
+
+        this.Icons = {
+            Success: { Class: "fa-check-circle-o", Color: "#54bb49" },
+            Exclamation: { Class: "fa-exclamation-triangle", Color: "#ff0" },
+            Failure: { Class: "fa-times-circle-o", Color: "#f00" },
+            Bug: { Class: "fa-bug", Color: "#000" }
+        };
+
+        this.Show = function (message, title, icon) {
+            if (typeof title == "undefined")
+                title = "";
+
+            if (typeof message == "undefined")
+                message = "";
+
+            /* Header */
+            var modalClose = document.createElement("span");
+            modalClose.className = "modal-close";
+            modalClose.innerHTML = "&times;";
+
+            var modalTitle = document.createElement("span");
+            modalTitle.innerText = title;
+
+            var modalHeader = document.createElement("div");
+            modalHeader.className = "modal-header";
+            modalHeader.appendChild(modalClose);
+            modalHeader.appendChild(modalTitle);
+
+            /* Body */
+            var modalMessage = document.createElement("p");
+            modalMessage.innerHTML = message;
+
+            var modalIcon = document.createElement("i");
+            modalIcon.className = "fa " + icon.Class + " modal-icon";
+            modalIcon.style.color = icon.Color;
+
+            var modalBody = document.createElement("div");
+            modalBody.className = "modal-body";
+            modalBody.appendChild(modalMessage);
+            modalBody.appendChild(modalIcon);
+
+            /* Buttons */
+            var button = document.createElement("button");
+            button.className = "modal-button";
+            button.innerText = "OK";
+
+            var modalFooter = document.createElement("div");
+            modalFooter.className = "modal-footer";
+            modalFooter.appendChild(button);
+
+            /* Modal base */
+            var modalContent = document.createElement("div")
+            modalContent.className = "modal-content";
+            modalContent.appendChild(modalHeader);
+            modalContent.appendChild(modalBody);
+            modalContent.appendChild(modalFooter);
+
+            var modal = document.createElement("div");
+            modal.className = "modal";
+            modal.appendChild(modalContent);
+            modal.style.display = "block";
+
+            /*trigger events*/
+            var closeModalEvent = function () {
+                document.body.removeChild(modal);
+                window.removeEventListener("click", closeModalEvent)
+            };
+
+            modalClose.onclick = closeModalEvent;
+            button.onclick = closeModalEvent;
+            //window.addEventListener("click", closeModalEvent); - bug
+
+            document.body.appendChild(modal);
+            button.focus();
+        }
+
+        return this;
+    }();
+
+    return this;
+}();
+
+
+//<div id="myModal" class="modal">
+//    <div class="modal-content">
+//        <div class="modal-header">
+//            <span class="close">&times;</span>
+//            <h2>Modal Header</h2>
+//        </div>
+//        <div class="modal-body">
+//            <p>Some text in the Modal Body</p>
+//            <p>Some other text...</p>
+//        </div>
+//        <div class="modal-footer">
+//            <h3>Modal Footer</h3>
+//        </div>
+//    </div>
+//</div>
