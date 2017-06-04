b49cf3be693fd5431508dc2d12fd87d20c4eb089
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2017-06-04 12:47:37 -0300

    Added portuguese page for fun

---
+++ html-templates/Client/scripts.js
@@ -0,0 +1,45 @@
+﻿/// <reference path="E:\Projects\html-templates\html-templates\tarks.js" />
+
+function toggleNav() {
+    if (document.getElementById("mySidenav").style.width == "250px") {
+        document.getElementById("mySidenav").style.width = "0px";
+    }
+    else {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+}
+
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
+
+var topnav = document.querySelector(".topnav");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 120) {
+        topnav.classList.add("topnav-scrolled");
+        document.body.style.paddingTop = topnav.offsetHeight + "px";
+    }
+    else {
+        topnav.classList.remove("topnav-scrolled");
+        document.body.style.paddingTop = "0px";
+    }
+}
+
+var countryLinks = document.querySelectorAll(".card-subtitle a");
+
+for (var i = 0; i < countryLinks.length; i++) {
+    countryLinks[i].onclick = function () {
+        Tarks.MessageBox.Show(
+            "You have clicked in one country link",
+            "Country",
+            Tarks.MessageBox.Icons.Success
+        );
+        return false;
+    }
+}
