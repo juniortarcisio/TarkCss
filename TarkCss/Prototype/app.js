d27f096211a16d575316f325e2f385c936b0384f
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2017-06-10 23:46:15 -0300

    Added many angular tests

---
+++ TarkCss/Prototype/app.js
@@ -0,0 +1,65 @@
+﻿var app = angular.module('myApp', ["ngRoute"]);
+app.controller('serversCtrl', function ($scope, $http) {
+
+    $http.get("http://localhost:50907/Server/1")
+    .then(function (response) {
+        $scope.servers = response.data;
+    }, function myError(response) {
+        $scope.servers = response.statusText;
+    });
+
+    $scope.SelectServer = function(server) {
+        $scope.selectedServer = server;
+        $scope.selectedName = server.Name;
+    };
+
+    //$http({
+    //    method: "GET",
+    //    url: "welcome.htm"
+    //}).then(function mySuccess(response) {
+    //    $scope.results = response.data;
+    //}, function myError(response) {
+    //    $scope.results = response.statusText;
+    //});
+
+});
+
+
+//app.controller('simplePresentCtrl', function ($scope, $rootScope) {
+//    $rootScope.title = "SimplePresent";
+//});
+
+//app.controller('toBeCtrl', function ($scope, $rootScope) {
+//    $rootScope.title = "To be verb";
+//});
+
+//app.controller('settingsCtrl', function ($scope, $rootScope) {
+//    $rootScope.title = "Settings";
+//});
+
+//app.controller('aboutCtrl', function ($scope, $rootScope) {
+//    $rootScope.title = "About";
+//});
+
+//app.controller('homeCtrl', function ($scope, $rootScope) {
+//    $rootScope.title = "Home";
+//});
+
+//app.config(function ($routeProvider) {
+//    $routeProvider
+//    .when("/SimplePresent", {
+//        templateUrl: "portuguese_present.html", controller: "simplePresentCtrl"
+//    })
+//    .when("/ToBe", {
+//        templateUrl: "portuguese_tobe.html", controller: "toBeCtrl"
+//    })
+//    .when("/Settings", {
+//        templateUrl: "portuguese_settings.html", controller: "settingsCtrl"
+//    })
+//    .when("/About", {
+//        templateUrl: "portuguese_about.html", controller: "aboutCtrl"
+//    })
+//    .otherwise({
+//        templateUrl: "portuguese_home.html", controller: "homeCtrl"
+//    });
+//});
---
+++ TarkCss/Responsive/portuguese_app.js
@@ -0,0 +1,71 @@
+﻿var app = angular.module('myApp', ["ngRoute"]);
+app.controller('regularVerbsCtrl', function ($scope, $http) {
+    $scope.verbs = [
+        { Portuguese: "Andar", English: "Walk" },
+        { Portuguese: "Comer", English: "Eat" },
+        { Portuguese: "Beber", English: "Drink" },
+        { Portuguese: "Comprar", English: "Buy" },
+        { Portuguese: "Nadar", English: "Swim" },
+        { Portuguese: "Cantar", English: "Sing" },
+        { Portuguese: "Falar", English: "Speak" },
+        { Portuguese: "Entender", English: "Understand" },
+        { Portuguese: "Trabalhar", English: "Work" },
+        { Portuguese: "Estudar", English: "Study" },
+        { Portuguese: "Aprender", English: "Learn" },
+        { Portuguese: "Jogar", English: "Play" }
+    ];
+
+    $scope.selectedVerb = $scope.verbs[0];
+
+    $scope.format = function (verb) {
+        return verb.English + ' -> ' + verb.Portuguese;
+    };
+
+});
+
+app.filter('RemoveLastChar', function () {
+    return function (x, q) {
+        if (typeof q == "undefined")
+            q = 1;
+        return x.substr(0, x.length - q);
+    };
+});
+
+app.controller('simplePresentCtrl', function ($scope, $rootScope) {
+    $rootScope.title = "SimplePresent";
+});
+
+app.controller('toBeCtrl', function ($scope, $rootScope) {
+    $rootScope.title = "To be verb";
+});
+
+app.controller('settingsCtrl', function ($scope, $rootScope) {
+    $rootScope.title = "Settings";
+});
+
+app.controller('aboutCtrl', function ($scope, $rootScope) {
+    $rootScope.title = "About";
+});
+
+app.controller('homeCtrl', function ($scope, $rootScope) {
+    $rootScope.title = "Home";
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/SimplePresent", {
+        templateUrl: "portuguese_present.html", controller: "simplePresentCtrl"
+    })
+    .when("/ToBe", {
+        templateUrl: "portuguese_tobe.html", controller: "toBeCtrl"
+    })
+    .when("/Settings", {
+        templateUrl: "portuguese_settings.html", controller: "settingsCtrl"
+    })
+    .when("/About", {
+        templateUrl: "portuguese_about.html", controller: "aboutCtrl"
+    })
+    .otherwise({
+        templateUrl: "portuguese_home.html", controller: "homeCtrl"
+    });
+});
