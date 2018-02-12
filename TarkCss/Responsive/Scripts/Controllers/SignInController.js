029b36019223bbefcdd474b1a9accd3d6fa99d35
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2018-02-11 23:28:32 -0200

    More prototyping progress

--- TarkCss/Responsive/Scripts/Controllers/SignInController.js
+++ TarkCss/Responsive/Scripts/Controllers/SignInController.js
@@ -1,3 +1,15 @@
-﻿app.controller('signInCtrl', function ($scope, $rootScope) {
+﻿app.controller('signInCtrl', function ($scope, $rootScope, AuthenticationService) {
     $rootScope.title = "Sign In";
+
+    $scope.signedUp = function (response) {
+        alert('ok you were signed in, but it\'s on construction');
+    }
+
+    $scope.errorCallback = function (response) {
+        alert('error: ' + response);
+    }
+
+    $scope.submit = function () {
+        AuthenticationService.SignIn($scope.user, $scope.signedUp, $scope.errorCallback);
+    }
 });
--- TarkCss/Responsive/Scripts/Controllers/SignUpController.js
+++ TarkCss/Responsive/Scripts/Controllers/SignUpController.js
@@ -1,7 +1,15 @@
 ﻿app.controller('signUpCtrl', function ($scope, $rootScope, AuthenticationService) {
     $rootScope.title = "Sign Up";
 
+    $scope.signedUp = function (response) {
+        alert('ok you were signed in, but it\'s on construction');
+    }
+
+    $scope.errorCallback = function (response) {
+        alert('error: ' + response);
+    }
+
     $scope.submit = function () {
-        AuthenticationService.SignUp($scope.signUp);
+        AuthenticationService.SignUp($scope.newUser, $scope.signedUp, $scope.errorCallback);
     }
 });
