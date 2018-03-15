6f85357752b4c56a8c727115fd6712b5e48fac40
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2018-03-15 01:02:04 -0300

    Fixing branch

---
+++ TarkCss/Responsive/PageHeader/pageHeader.js
@@ -0,0 +1,13 @@
+app.controller('pageHeaderCtrl', function () {
+
+
+});
+
+
+app.component('pageHeader', {
+    bindings: {
+    },
+    scope: true,
+    templateUrl: 'PageHeader/pageHeader.html',
+    controller: 'pageHeaderCtrl'
+}); 
