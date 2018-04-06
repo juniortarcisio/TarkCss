04164ae4725dd4cf6978441f7ea3d77322da3346
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2018-04-05 22:51:55 -0300

    Added small language helper

--- TarkCss/Responsive/PageHeader/pageHeader.js
+++ TarkCss/Responsive/PageHeader/pageHeader.js
@@ -1,17 +1,17 @@
-app.controller('pageHeaderCtrl', function ($scope, $rootScope) {
+app.controller('pageHeaderCtrl', function ($scope, $rootScope, $timeout) {
     $scope.selectLanguageFrom = function (language) {
         $rootScope.langFrom = language;
         localStorage.langFrom = $rootScope.langFrom.id;
-
-        var audio = new Audio('../Media/blop.mp3');
-        audio.play();
+        
+        new Audio('../Media/blop.mp3').play();
+        $rootScope.hideLangHelp();
     }
     $scope.selectLanguageLearn = function (language) {
         $rootScope.langLearn = language;
         localStorage.langLearn = $rootScope.langLearn.id;
-
-        var audio = new Audio('../Media/blop.mp3');
-        audio.play();
+        
+        new Audio('../Media/blop.mp3').play();
+        $rootScope.hideLangHelp();
     }
 
     $scope.invertLanguages = function () {
@@ -22,9 +22,28 @@ app.controller('pageHeaderCtrl', function ($scope, $rootScope) {
         localStorage.langFrom = $rootScope.langFrom.id;
         localStorage.langLearn = $rootScope.langLearn.id;
 
-        var audio = new Audio('../Media/blop.mp3');
-        audio.play();
+        new Audio('../Media/blop.mp3').play();
+        $rootScope.hideLangHelp();
     }
+
+    $rootScope.hideLangHelp = function (playSound) {
+        $rootScope.langHelp = false;
+
+        if (playSound)
+            new Audio('../Media/blop.mp3').play();
+    }
+    $rootScope.showLangHelp = function (playSound) {
+        $rootScope.langHelp = true;
+
+        if ($rootScope.prmsHiddingLangHelp)
+            $timeout.cancel($rootScope.prmsHiddingLangHelp);
+
+        $rootScope.prmsHiddingLangHelp = $timeout($rootScope.hideLangHelp, 7000);
+
+        if (playSound)
+            new Audio('../Media/blop.mp3').play();
+    }
+    $rootScope.showLangHelp();
 });
 
 app.component('pageHeader', {
