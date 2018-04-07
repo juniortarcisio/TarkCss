729cc18e771028202498aff0bda522bb9a576182
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2018-04-06 22:48:51 -0300

    Improved how to and small menu fix.

--- TarkCss/Responsive/PageMenu/pageMenu.js
+++ TarkCss/Responsive/PageMenu/pageMenu.js
@@ -47,7 +47,7 @@
         },
         {
             name: ['Beginning', 'Iniciando', 'Awal'], closed: false, items: [
-                { name: ['How to Learn', 'Como Aprender', 'Bagaimana belajar'], href: '#!Beginning/HowToLearn', icon: 'fa-book' },
+                { name: ['How To Learn', 'Como Aprender', 'Bagaimana belajar'], href: '#!Beginning/HowToLearn', icon: 'fa-book' },
                 { name: ['To Be Verb', 'Verbo To be', 'Kata Kerja To Be'], href: '#!Beginning/ToBeVerb', icon: 'fa-book' }
             ]
         },
