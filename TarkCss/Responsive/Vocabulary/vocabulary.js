18c5c5298c5600ac57233f974c9f5461b9b54dd3
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2018-04-08 22:24:01 -0300

    Added words and decks counting

--- TarkCss/Responsive/Vocabulary/vocabulary.js
+++ TarkCss/Responsive/Vocabulary/vocabulary.js
@@ -19,4 +19,19 @@
         return "(" + obs + ")";
     }
 
+    $scope.calculateDeckAndWords = function () {
+        $scope.totalDecks = 0;
+        $scope.totalWords = 0;
+
+        for (var i = 0; i < $scope.albums.length; i++){
+            $scope.totalDecks += $scope.albums[i].decks.length;
+
+            for (var j = 0; j < $scope.albums[i].decks.length; j++) 
+                $scope.totalWords += $scope.albums[i].decks[j].words.length;
+        }
+
+        
+    };
+
+    $scope.calculateDeckAndWords();
 });
