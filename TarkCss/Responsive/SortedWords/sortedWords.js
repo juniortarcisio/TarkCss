37cc4c496dd382173e28ff780fa1b4e4e7a17bf6
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2018-04-15 00:26:46 -0300

    Added condition to ignore math words and added var with similarityLevel on Similar words

--- TarkCss/Responsive/SortedWords/sortedWords.js
+++ TarkCss/Responsive/SortedWords/sortedWords.js
@@ -8,6 +8,11 @@
         for (var i = 0; i < $scope.albums.length; i++) {
             for (var j = 0; j < $scope.albums[i].decks.length; j++) {
                 for (var k = 0; k < $scope.albums[i].decks[j].words.length; k++) {
+
+                    //Ignore math numbers
+                    if (i == 2 && j < 4)
+                        continue;
+
                     var word = $scope.albums[i].decks[j].words[k];
                     word.albumName = $scope.albums[i].name;
                     word.deckName = $scope.albums[i].decks[j].name;
@@ -28,22 +33,25 @@
         
         var similarWordsGroups = new Array();
         var currentGroupPrefix = ' ';
+        var similarityLevel = 3;
             
         for (var i = 0; i < orderedWords.length; i++) {
-            if (orderedWords[i].lang[$rootScope.langLearn.id].length < 3)
+            if (orderedWords[i].lang[$rootScope.langLearn.id].length < similarityLevel)
                 continue;
 
-            var currentWordPrefix = orderedWords[i].lang[$rootScope.langLearn.id].substring(0, 2);
+            var currentWordPrefix = orderedWords[i].lang[$rootScope.langLearn.id].substring(0, similarityLevel);
 
             //Similarity rule here...
             if (currentWordPrefix != currentGroupPrefix) {
 
                 //Checks if there is a next word and at least the next word is similar
                 if (i < orderedWords.length - 1) {
-                    var nextWordPrefix = orderedWords[i + 1].lang[$rootScope.langLearn.id].substring(0, 2);
+                    var nextWordPrefix = orderedWords[i + 1].lang[$rootScope.langLearn.id].substring(0, similarityLevel);
 
                     if (currentWordPrefix != nextWordPrefix)
                         continue;
+                } else {
+                    continue;
                 }
 
                 currentGroupPrefix = currentWordPrefix;
